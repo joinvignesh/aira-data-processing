e1b71d4a1d4dbd3b108ce6d338fe334740ac96be
vignesh_br <vigneshbr12@gmail.com>
2026-04-19 11:34:10 +0530

    implemented redis client to connect wioth redis container

---
+++ app/core/redis_client.py
@@ -0,0 +1,11 @@
+import os
+import redis
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+def get_redis():
+    client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+    try:
+        yield client
+    finally:
+        client.close()
